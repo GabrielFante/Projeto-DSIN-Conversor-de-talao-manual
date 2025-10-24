f16d7c21058c1ba9ece5ae25ecfcd4b8c9bc1ce8
GabrielFante <164691063+GabrielFante@users.noreply.github.com>
2025-10-24 18:54:44 -0300

    Delete Back-end/Back-end directory

--- Back-end/Back-end/DSIN.Bussiness/Interfaces/IRepositories/IAgentRepository.cs
+++
@@ -1,9 +0,0 @@
-using DSIN.Bussiness.Models;
-
-namespace DSIN.Bussiness.Interfaces.Repositories;
-
-public interface IAgentRepository
-{
-    Task<Agent?> GetByIdAsync(Guid id, CancellationToken ct);
-    Task AddAsync(Agent agent, CancellationToken ct);
-}
--- Back-end/Back-end/DSIN.Bussiness/Interfaces/IRepositories/IVehicleRepository.cs
+++
@@ -1,9 +0,0 @@
-using Dsin.Ticketing.Domain.Entities;
-
-namespace Dsin.Ticketing.Domain.Ports.Repositories;
-
-public interface IVehicleRepository
-{
-    Task<Vehicle?> GetByPlateAsync(string plate, CancellationToken ct);
-    Task AddAsync(Vehicle vehicle, CancellationToken ct);
-}
